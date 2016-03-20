6a8113abaccac1ed2697b433910352e0e456e1de
Madelene Campos <madelenecampos@gmail.com>
2016-03-20 15:55:48 -0400

    rspec tests run without failures

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+require 'api_constraints'
+
 MarketPlaceApi::Application.routes.draw do 
   #Api definition
-  namespace :api, defaults: { format: :json },
-                              constraints: { subdomain: 'api' }, path: '/' do
-    #This is where we list our resources
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      #This is where we list our resources
+    end
   end
 end
