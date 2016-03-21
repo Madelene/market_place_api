2aee7a30b8d29670b3d4526e6596fd91c68621ae
Madelene Campos <madelenecampos@gmail.com>
2016-03-21 11:01:40 -0400

    Updated app controller to prevent CSRF exception from being raised and removed subdomain from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 require 'api_constraints'
 
 MarketPlaceApi::Application.routes.draw do 
+
   devise_for :users
   #Api definition
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json } do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       #This is where we list our resources
-      resources :users, :only => [:show]
+      resources :users, only: [:show]
     end
   end
 end
